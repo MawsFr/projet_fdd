e44dbef925ebeaa8995caf4fde31af37ef720bff
Mathieu <Mathieu@mathieu.com>
2017-02-16 16:20:43 +0100

    WIP genres

--- src/Parser.java
+++ src/Parser.java
@@ -1,9 +1,15 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 import extensions.CSVFile;
 
 
 public class Parser {
 
 	private int[] stringColumns = {1, 2, 7, 10, 11, 12, 15, 17, 18, 20, 21, 22};
+	private Set<String> movieGenres = new HashSet();
 	
 	/**
 	 * @param args
@@ -13,12 +19,12 @@ public class Parser {
 			new Parser().processFile(args[0]);
 		} else {
 			new Parser().processFile("movie_metadata.csv");
-			
 		}
 	}
 	
 	public void processFile(String name) {
-		this.processStrings(name, ',');
+		//this.processStrings(name, ',');
+		this.createMovieGenres(name, ',');
 	}
 	
 	public void processStrings(String name, char separator) {
@@ -30,6 +36,33 @@ public class Parser {
 			System.out.println();
 		}
 		
+	}
+	
+	public void createMovieGenres(String name, char separator) {
+		CSVFile file = new CSVFile(name, separator);
+		for(int i=1;i<file.rowCount();i++) {
+			String genres = file.getCell(i, 9);
+			System.out.println("Movie title : " + file.getCell(i, 11));
+			System.out.println("Movie genres : " + genres);
+			/*
+			 * String string = "004-034556";
+			String[] parts = string.split("-");
+			String part1 = parts[0]; // 004
+			String part2 = parts[1]; // 034556
+			 */
+			String[] parts = genres.split("\\|");
+			for(int j = 0;j<parts.length;j++) {
+				if(!movieGenres.contains(parts[j]))
+					movieGenres.add(parts[j]);
+				else
+					System.out.println(parts[j] + " skipped");
+			}
+			System.out.println("movie genres array size : " + movieGenres.size());
+		}		
+		for (Iterator iterator = movieGenres.iterator(); iterator.hasNext();) {
+			String string = (String) iterator.next();
+			System.out.println(string);
+		}
 	}
 
 }
